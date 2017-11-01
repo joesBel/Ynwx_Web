347d74e1a5b7c0d5584be3e618fd9e97cd1225a0
itachi0729 <996364333@qq.com>
2017-11-01 09:58:23 +0800

    轮播

--- yinuo/app/js/baseControllers.js
+++ yinuo/app/js/baseControllers.js
@@ -2,8 +2,28 @@
 
 /* created by zhoupeng 2017-10-27 */
 
-app.controller('baseController',['$scope','$state',function($scope,$state){
-    $scope.goState = function(route){
-        $state.go(route);
-    }
+app.controller('baseController',['$scope','$state','$timeout', function($scope,$state,$timeout){
+    /*  $timeout(function(){
+          $('.shutter').shutter({
+              shutterW: 1000, // 容器宽度
+              shutterH: 358, // 容器高度
+              isAutoPlay: true, // 是否自动播放
+              playInterval: 3000, // 自动播放时间
+              curDisplay: 3, // 当前显示页
+              fullPage: false // 是否全屏展示
+          });
+      },1000);*/
+
+    $(function () {
+        $('.shutter').shutter({
+            shutterW: 1000, // 容器宽度
+            shutterH: 358, // 容器高度
+            isAutoPlay: true, // 是否自动播放
+            playInterval: 3000, // 自动播放时间
+            curDisplay: 3, // 当前显示页
+            fullPage: false // 是否全屏展示
+        });
+    });
+
+
 }]);
